96b591687514f4c1dff6ca45317c8a71b26d2697
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-06-08 14:11:44 +0100

    create dog controller methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :dogs, :favourites, :reviews
+  has_many :dogs
+  has_many :favourites
+  has_many :reviews
   has_many :bookings, through: :dogs
   # validates :name, presence: true
   # validates :address, presence: true

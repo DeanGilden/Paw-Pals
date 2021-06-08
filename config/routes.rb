a6823969f22d30f4bcf5b471e873534041dfcbe7
Dean Gilden <dean.gilden@hotmail.co.uk>
2021-06-08 14:11:29 +0100

    routes and updated models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dogs do
+    resources :bookings, only: [ :new, :create ]
+    resources :reviews, only: [ :new, :create ]
+    resources :favourites, only: [ :new, :create ]
+    end
+  resources :bookings, only: [ :index, :show, :destroy ]
+  resources :reviews, only: [ :destroy ]
+  resources :users, only: [ :show, :destroy ]
+  resources :favourites, only: [ :destroy ]
 end

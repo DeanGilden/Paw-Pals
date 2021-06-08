9ea9c4ce5e60121c393ef00acb81ba5df3c90e64
yonatan rospigliosi <jonrospi@hotmail.com>
2021-06-08 17:10:51 +0100

    created reviews controller

---
+++ app/controllers/favourites_controller.rb
@@ -0,0 +1,22 @@
+class FavouritesController < ApplicationController
+  def new
+    @favourite = Favourite.new
+  end
+
+  def create
+    @favourite = Favourite.new()
+    @favourite.dog_id = params[:dog_id]
+    @favourite.user = current_user
+    if @favourite.save
+      redirect_to dog_path(params[:dog_id])
+    else
+      render 'dogs/show'
+    end
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+    redirect_to user_path(current_user)
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  def new
+   @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @dog = dog.find(params[:dog_id])
+    @review.dog = @dog
+    @review.user = current_user
+    if @review.save
+      redirect_to dog_path(params[:dog_id])
+    else
+      render :new
+    end
+  end
+  def destroy
+    @review.destroy
+    redirect_to dog_path(params[:dog_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end

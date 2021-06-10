082c153ba5a0e7f7943ed4cf5e628081970cc6de
Dean Gilden <dean.gilden@hotmail.co.uk>
2021-06-10 17:55:56 +0100

    Started temperments bar + checklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_10_140353) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,9 +97,9 @@ ActiveRecord::Schema.define(version: 2021_06_10_140353) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "dogs", column: "my_dog_id"
   add_foreign_key "bookings", "dogs", column: "their_dog_id"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "dogs", "users"
   add_foreign_key "favourites", "dogs"
   add_foreign_key "favourites", "users"

79cc66284c43cbce22b53b07903da99f0f04c565
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-06-15 16:30:19 +0100

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
   has_many :my_dogs_bookings, through: :dogs, source: :my_bookings
   has_many :bookings_for_other_dogs, through: :dogs, source: :their_bookings
   has_many :chatrooms, dependent: :destroy
-  has_many :other_chatrooms, foreign_key: "other_user_id", class_name: "User"
-
+  has_many :other_chatrooms, foreign_key: "other_user_id", class_name: "Chatroom", dependent: :destroy
 
   # validates :name, :avatar, :address, presence: true
   # validates :contact_number, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+# Message.destroy_all
+# Chatroom.destroy_all
 Booking.destroy_all
 Dog.destroy_all
 User.destroy_all

a2004be754da3b75ecd3447da1505d4f869568aa
Joe Smith <joeycrushed@gmail.com>
2021-06-16 14:22:24 +0100

    Booking Cards & PawPal Req Card & New My Request Page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-
+  config.assets.check_precompiled_asset = false
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -60,4 +60,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
 end
